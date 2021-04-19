335700157486faf5be3c7e93d3ac6d99c904a5d4
muniz <1160007652@qq.com>
2021-04-19 15:27:47 +0800

    feat: update plugin

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-transform-runtime'],
+    plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties'],
   };
 };
 // , '@babel/plugin-syntax-top-level-await'
--- src/index.js
+++ src/index.js
@@ -9,32 +9,31 @@ import i18n from './configs/i18n';
 // 初始化多语言
 i18n.initI18n();
 
-class Life {
-  constructor() {
-    this.isStart = true; // 安装成功后是否立即执行, 目前仅在 Mac 系统下有效
-  }
+class PluginRoot {
+  static isStart = true; // 安装成功后是否立即执行
+  static autoUpgrade = true; // 是否动态感知最新版本，以实现自动升级
+
+  /**
+   * 静态方法，不可修改 muniz 脚手架（宿主环境）通过此通道执行插件命令
+   * @param {object} param
+   * @param {string} param.commandType 命令类型，react function
+   * @param {string} param.commandPath 执行命令路径
+   * @param {object} param.data 插件命令数据
+   */
+  static runPluginCommand = async ({ commandType, commandPath, data }) => {
+    const _command = require(`./command/${commandPath}`).default;
+    console.log(commandType);
+    if (commandType === 'function') {
+      _command(data);
+    } else {
+      render(React.createElement(_command, data));
+    }
+  };
 }
 
-export const pluginLife = (props) => {
+export default (props) => {
   const { locale } = props;
   // 初始化多语言
   i18n.setLocale({ locale });
-  return new Life();
-};
-
-/**
- * muniz 脚手架（宿主环境）通过此通道执行插件命令
- * @param {object} param
- * @param {string} param.commandType 命令类型，react function
- * @param {string} param.commandPath 执行命令路径
- * @param {object} param.data 插件命令数据
- */
-export const pluginCommand = ({ commandType, commandPath, data }) => {
-  const _command = require(`./command/${commandPath}`).default;
-  console.log(commandType);
-  if (commandType === 'function') {
-    _command(data);
-  } else {
-    render(React.createElement(_command, data));
-  }
+  return PluginRoot;
 };
